dca72ff33d174f288f4cab525883e805504f0737
Daniel-Piasecki <daniel.piasceki03@gmail.com>
2024-05-30 12:27:50 +0200

    1 section

---
+++ src/Basic.java
@@ -0,0 +1,15 @@
+public class Basic {
+    public static void test() {
+        System.out.println("This is");
+        System.out.println("test");
+
+        // comment
+        /*
+        also
+        comment
+         */
+        
+
+
+    }
+}
