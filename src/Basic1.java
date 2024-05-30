c83308b2c2a1ae63dc4be91cbcbb08adf67254b6
Daniel-Piasecki <daniel.piasceki03@gmail.com>
2024-05-30 12:42:27 +0200

    variables 1

---
+++ src/Basic1.java
@@ -0,0 +1,22 @@
+public class Basic1 {
+    public static void test(){
+        int x; // declaration
+        x = 123; // assignment
+
+        int y = 321; //initialization
+        System.out.println("My Numbers is: "+x+" "+y);
+        double z = 3.14;
+        float k = 3.1444f;
+
+        System.out.println(k+" "+z);
+        boolean t = true; //true or false
+        System.out.println(t);
+        char symbol = '@';
+        System.out.println(symbol);
+        String name = "Daniel";
+        System.out.println("Hello "+name);
+
+
+
+    }
+}
