da921fb8d54312f7b86bd70f309085fbb6a5f945
Daniel-Piasecki <daniel.piasceki03@gmail.com>
2024-06-01 12:46:12 +0200

    for

---
+++ src/Basic12.java
@@ -0,0 +1,9 @@
+public class Basic12 {
+    public static void test(){
+
+        for(int i = 10; i>=0; i--){
+            System.out.println(i);
+        }
+        System.out.println("Happy new year!");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Basic.test();
-        Basic1.test();
-        Basic2.test();
-        //Basic3.test();
-        System.out.println("-------------");
-        Basic4.test();
-        System.out.println("--------------");
-        //Basic5.test();
-        System.out.println("--------------");
-        //Basic6.test();
-        System.out.println("--------------");
-        Basic7.test();
-        System.out.println("--------------");
-        Basic8.test();
-        System.out.println("--------------");
-        Basic9.test();
-//        System.out.println("--------------");
-//        Basic10.test();
-        System.out.println("--------------");
-        Basic11.test();
+        Basic12.test();
     }
 }
