9d64b7fb228b5245a8e889b533998ee532bd66e4
Daniel-Piasecki <daniel.piasceki03@gmail.com>
2024-07-31 12:24:08 +0200

    arrays

---
+++ src/Basic15.java
@@ -0,0 +1,24 @@
+public class Basic15 {
+    public static void test(){
+
+        String[][] cars = new String[3][3];
+
+        cars[0][0] = "Camaro";
+        cars[0][1] = "Corvette";
+        cars[0][2] = "Tesla";
+        cars[1][0] = "Zygzak";
+        cars[1][1] = "Sally";
+        cars[1][2] = "Ferrari";
+        cars[2][0] = "Lamnbo";
+        cars[2][1] = "Toyota";
+        cars[2][2] = "Skoda";
+
+        for (int i = 0; i < cars.length ; i++) {
+            System.out.println();
+            for (int j = 0; j <cars[i].length ; j++) {
+                System.out.println(cars[i][j] + " ");
+            }
+        }
+
+    }
+}
