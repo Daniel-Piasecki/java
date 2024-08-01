a09850efe7db9599b6321611b4de8da424659adc
Daniel-Piasecki <daniel.piasceki03@gmail.com>
2024-08-01 21:56:33 +0200

    wrappers

---
+++ src/Basic17.java
@@ -0,0 +1,13 @@
+public class Basic17 {
+    public static void test() {
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "Daniel";
+        if (b == '@') {
+            System.out.println("This is true");
+
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Basic16.test();
+        Basic17.test();
     }
 }
