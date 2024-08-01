df4d9b2c0441294f12db91561c6936d93096d6f5
Daniel-Piasecki <daniel.piasceki03@gmail.com>
2024-08-01 21:45:03 +0200

    arrays

---
+++ src/Basic16.java
@@ -0,0 +1,20 @@
+public class Basic16 {
+    public static void test(){
+
+        String name = "Daniel";
+
+
+        //boolean result = name.equalsIgnoreCase("daniel");
+        //int result = name.length();
+        //char result = name.charAt(0);
+        //int result = name.indexOf("n");
+        //boolean result = name.isEmpty();
+//        String result = name.toUpperCase();
+//        String result = name.toLowerCase();
+        String result = name.replace('a','o');
+
+
+        System.out.println(result);
+
+    }
+}
