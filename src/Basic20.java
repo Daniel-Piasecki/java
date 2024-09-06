6d01e589c3d570b6e518f1533d1b16bbb2929017
Daniel-Piasecki <daniel.piasceki03@gmail.com>
2024-09-06 13:10:55 +0200

    Basic 21.1

---
+++ src/Basic20.java
@@ -0,0 +1,11 @@
+public class Basic20 {
+    public static void test() {
+        String name = "Daniel";
+        int age = 24;
+        hello(name,age);
+
+    }
+    static void hello(String FirstName, int number){
+        System.out.println("Hello" + " " + FirstName + " " + number + " years old");
+    }
+}
