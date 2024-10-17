69fffd6370e98ef2ca0bca04b0b98e87c113927f
Daniel-Piasecki <daniel.piasceki03@gmail.com>
2024-10-17 14:05:43 +0200

    Basic of OOP

---
+++ src/Basic24.java
@@ -0,0 +1,18 @@
+public class Basic24 {
+    public static void test(){
+
+    Basic24Car myCar1 = new Basic24Car();
+    Basic24Car myCar2 = new Basic24Car();
+
+
+    System.out.println(myCar1.make);
+    System.out.println(myCar1.model);
+
+
+
+    //myCar1.drive();
+        //myCar1.brake();
+
+
+    }
+}
---
+++ src/Basic24Car.java
@@ -0,0 +1,16 @@
+public class Basic24Car {
+
+    String make = "Skoda";
+    String model = "Octavia";
+    int year = 2015;
+    String color = "black";
+    double price = 10000.00;
+
+    void drive(){
+        System.out.println("You drive the car");
+    }
+    void brake(){
+        System.out.println("You step on the brakes!");
+    }
+
+}
